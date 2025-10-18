1a7bbdd94d8cff4e93d1cc484f8d85c706468911
w-o-r-l-d-w-i-d-e <dylanfmcconnell@gmail.com>
2025-10-17 23:59:36 -0400

    vercel-fix-1

--- app/Labs/Lab3/MapFunction.tsx
+++ app/Labs/Lab3/MapFunction.tsx
@@ -1,5 +1,5 @@
 export default function MapFunction() {
-  let numberArray1 = [1, 2, 3, 4, 5, 6];
+  const numberArray1 = [1, 2, 3, 4, 5, 6];
   const square = (a: number) => a * a;
   const todos = ["Buy milk", "Feed the pets"];
   const squares = numberArray1.map(square);
@@ -13,7 +13,7 @@ export default function MapFunction() {
       <h4>List of Todos</h4>
       <ol>
         {todos.map((todo) => (
-          <li>{todo}</li>
+          <li key={todo}>{todo}</li>
         ))}
       </ol>
       <hr/>
--- app/Labs/Lab3/VariableTypes.tsx
+++ app/Labs/Lab3/VariableTypes.tsx
@@ -1,20 +1,21 @@
 export default function VariableTypes() {
-  let numberVariable = 123;
-  let floatingPointNumber = 234.345;
-  let stringVariable = 'Hello World!';
-  let booleanVariable = true;
-  let isNumber = typeof numberVariable;
-  let isString = typeof stringVariable;
-  let isBoolean = typeof booleanVariable;
-  return(
+  const numberVariable = 123;
+  const floatingPointNumber = 234.345;
+  const stringVariable = "hello";
+  const booleanVariable = true;
+  const isNumber = typeof numberVariable === "number";
+  const isString = typeof stringVariable === "string";
+  const isBoolean = typeof booleanVariable === "boolean";
+  return (
     <div id="wd-variable-types">
-      <h4>Variables Types</h4>
-      numberVariable = { numberVariable }<br/>
-      floatingPointNumber = { floatingPointNumber }<br/>
-      stringVariable = { stringVariable }<br/>
-      booleanVariable = { booleanVariable + "" }<br/>
-      isNumber = { isNumber }<br/>
-      isString = { isString }<br/>
-      isBoolean = { isBoolean }<hr/>
+      <h4>Variable Types</h4>
+      numberVariable = {numberVariable} <br />
+      floatingPointNumber = {floatingPointNumber} <br />
+      stringVariable = {stringVariable} <br />
+      booleanVariable = {booleanVariable + ""} <br />
+      isNumber = {isNumber + ""} <br />
+      isString = {isString + ""} <br />
+      isBoolean = {isBoolean + ""} <hr />
     </div>
-);}
+  );
+}
