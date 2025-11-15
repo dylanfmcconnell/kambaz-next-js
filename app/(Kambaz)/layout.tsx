93f400a4d378a1157a89d4ed6112bcb7d9db3d7c
w-o-r-l-d-w-i-d-e <dylanfmcconnell@gmail.com>
2025-11-15 03:25:00 -0500

    session error

--- app/(Kambaz)/layout.tsx
+++ app/(Kambaz)/layout.tsx
@@ -1,29 +1,15 @@
 "use client";
 
-import { useSession } from "./Account/Session";
-import * as client from "./Account/client";
-import { useRouter } from "next/navigation";
+import { SessionProvider } from "./Account/Session";
 
-export default function KambazLayout({ children }: { children: React.ReactNode }) {
-  const { currentUser, setCurrentUser } = useSession();
-  const router = useRouter();
-
-  const signout = async () => {
-    await client.signout();
-    setCurrentUser(null);
-    router.push("/Account/Signin");
-  };
-
+export default function KambazLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className="container mt-4">
-      {currentUser && (
-        <div className="d-flex justify-content-end mb-3">
-          <button className="btn btn-outline-danger" onClick={signout}>
-            Signout
-          </button>
-        </div>
-      )}
-      {children}
-    </div>
+    <SessionProvider>
+      <div className="container mt-4">{children}</div>
+    </SessionProvider>
   );
 }
