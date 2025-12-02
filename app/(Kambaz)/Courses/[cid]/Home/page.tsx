00a321f85bdfbe6b6937321315cb3e6fcd1ec4c3
w-o-r-l-d-w-i-d-e <dylanfmcconnell@gmail.com>
2025-12-02 03:42:49 -0500

    param fix

--- app/(Kambaz)/Courses/[cid]/Home/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Home/page.tsx
@@ -1,18 +1,14 @@
 "use client";
 
-import ModulesWrapper from "../Modules/page";
+import Modules from "../Modules/page";
 import CourseStatus from "./Status";
 
-export default function Home({
-  params
-}: {
-  params: { cid: string };
-}) {
+export default function Home() {
   return (
     <div id="wd-home">
       <div className="d-flex">
         <div className="flex-fill me-3">
-          <ModulesWrapper params={params} />
+          <Modules />
         </div>
         <div className="d-none d-lg-block">
           <CourseStatus />
