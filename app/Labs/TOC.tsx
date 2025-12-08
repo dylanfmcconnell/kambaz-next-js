75d0d260f96da7de187c694e58426c6fbafaec6d
w-o-r-l-d-w-i-d-e <dylanfmcconnell@gmail.com>
2025-12-07 19:21:12 -0500

    change link

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -81,7 +81,7 @@ export default function TOC() {
       </NavItem>
       <NavItem>
         <NavLink
-          href="https://kambaz-node-server-app-a6-cbxr.onrender.com"
+          href="https://kambaz-node-server-app-2-n6on.onrender.com"
           as={Link}
           target="_blank"
         >
